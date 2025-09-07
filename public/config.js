85f3adee7c3cbc513d45726ee16cb41801fd4b70
alessandrodiazrodrigues <alessandrorodrigues@cvc.com.br>
2025-09-07 15:16:32 -0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,7 +6,7 @@
 
 const CVC_CONFIG = {
     // ✅ URL PRINCIPAL - ALTERAR APENAS AQUI quando reimplantar
-    API_URL: 'https://script.google.com/macros/s/AKfycbw4u_kgqqyRWWsz8SU-Dw6H4uJnb5ssiWpYZPE9P-TOe5O7f2llEtkmbxXiutJSj1M70A/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbzWnzbRXuI26_-3uZGe1H_9oDiucHGBe4e19JalI3vtb727PMXmODv3bu8O_rxB8B6FVw/exec',
     
     // 📊 INFORMAÇÕES DO SISTEMA
     VERSION: '8.021',
